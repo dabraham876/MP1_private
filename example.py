0bb20ec7c0a2fd8ef99ad8c8d67d09cf772a4063
dabraham876 <78887828+dabraham876@users.noreply.github.com>
2023-09-06 16:12:26 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,17 +1,19 @@
 import metapy
 
 def tokens_lowercase(doc):
-    #Write a token stream that tokenizes with ICUTokenizer (use it the argument "suppress_tags=True"), 
-    #lowercases, removes words with less than 2 and more than 5  characters
+    #Write a token stream that tokenizes with ICUTokenizer (use the argument "suppress_tags=True"), 
+    #lowercases, removes words with less than 2 and more than 5  characters. 
     #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
+    #Write a token stream that tokenizes with ICUTokenizer (use the argument "suppress_tags=True"),
     tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
-    #lowercases, removes words with less than 2 and more than 5  charactersl
+    #lowercases, removes words with less than 2 and more than 5  characters
     lowercase_filter = metapy.analyzers.LowercaseFilter(tok)
     length_filter = metapy.analyzers.LengthFilter(lowercase_filter, min=2, max=5)
     #performs stemming and creates trigrams (name the final call to ana.analyze as "trigrams")
     stemmer = metapy.analyzers.Porter2Filter(length_filter)
     trigram = metapy.analyzers.NGramWordAnalyzer(3, stemmer)
-    trigrams = trigram.analyze(doc)    
+    trigrams = trigram.analyze(doc)
+    
     #leave the rest of the code as is
     tok.set_content(doc.content())
     tokens, counts = [], []
